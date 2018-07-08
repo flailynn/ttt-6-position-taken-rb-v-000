2d801ba88809b38ae998aafb6ff54a0af24cafa7
Anita Beauchamp <amadamspr@gmail.com>
2018-07-08 22:41:31 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,9 @@
 # code your #position_taken? method here!
 def position_taken? (board, index)
   #some code
-  if board[index].include?("X") || board[index].include?("O")
+  if board[index]
+    false 
+  elsif board[index].include?("X") || board[index].include?("O")
     true
   else
     false
