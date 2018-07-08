f6907517a92d29254b54b0a40198a2952bdf5358
Anita Beauchamp <amadamspr@gmail.com>
2018-07-08 22:56:13 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@ def position_taken? (board, index)
   if board[index].nil?
     false
     #false
-  elsif (board[index].include? "X") || (board[index].include? "O")
+  elsif (board[index].upcase.include? "X") || (board[index].include? "O")
     # || board[index].include? "O"
     true
   else
